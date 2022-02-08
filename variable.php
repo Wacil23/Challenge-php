7f7b910b0df1fe43605eae56840f4f9db97fc365
Wacil23 <wacilzek00@gmail.com>
2022-02-08 11:49:10 +0100

    Php variable

--- variable.php
+++ variable.php
@@ -1,2 +1,14 @@
 <?php
-    
+    $bookName = 'Voyage avec un ane dans les Cévennes';
+    $read = TRUE;
+    $releaseBook = 1879;
+    $price = 6;
+    echo "$bookName<br />";
+    if ($read == TRUE) {
+        echo "Je l'ai lu<br />";
+    }
+    else {
+        echo "Pas lu<br />";
+    }
+    echo "$releaseBook<br />";
+    echo "$price";
